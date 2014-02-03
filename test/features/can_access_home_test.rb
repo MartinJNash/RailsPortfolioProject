441c72a57f6447baa23fa093977c22d0dc5450a1
Martin Nash <martin.j.nash@gmail.com>
2014-02-02 23:12:53 -0800

    added check for zurb test. closes #7.

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -3,7 +3,6 @@ require "test_helper"
 feature "CanAccessHome" do
   scenario "can access home" do
     visit "/"
-    page.must_have_content "Welcome"
-    page.wont_have_content "Goobye"
+    page.must_have_content "Martin"
   end
 end
---
+++ test/features/check_for_zurb_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+feature "Check For Zurb" do
+  scenario "the test is sound" do
+
+    visit root_path
+    page.must_have_content "columns"
+  end
+end

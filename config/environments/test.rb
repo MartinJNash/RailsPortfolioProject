2ac7f69e473023f11533575a6d264afa3e5fbb8c
Martin Nash <martin.j.nash@gmail.com>
2014-02-04 21:37:47 -0800

    broke out some view components into partials.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,8 @@ Portfolio::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+
+  # Blow up assets
+  config.assets.debug = true
 end
--- test/features/check_for_zurb_test.rb
+++ test/features/check_for_zurb_test.rb
@@ -3,6 +3,13 @@ require "test_helper"
 feature "Check For Zurb" do
   scenario "confirm that the layout is in Zurb" do
     visit root_path
-    page.must_have_css(".columns")
+
+    page.must_have_css(".row .columns")
+    page.source.must_include("foundation.js")
+    page.source.must_include("foundation_and_overrides.css")
+
+    # Must have
+    # config.assets.debug = true
+    # in test.rb
   end
 end

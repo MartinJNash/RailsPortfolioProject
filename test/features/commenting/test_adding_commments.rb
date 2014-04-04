29c73a57bcd74c84b50dbd91457457c8978d7a25
Martin Nash <martin.j.nash@gmail.com>
2014-04-04 01:04:15 -0700

    DRYed up adding comments tests

--- test/features/commenting/test_adding_commments.rb
+++ test/features/commenting/test_adding_commments.rb
@@ -1,40 +1,46 @@
 require 'test_helper'
 
-feature 'As a site owner, I want to allow people to comment on posts' do
-  
+# As a site owner, 
+# I want to allow logged in users to comment on posts, 
+# So that we can have a civil exchange of opinions between mature adults.
 
-  scenario 'Adding a valid comment' do
-    test_sign_in(:editor)
-    post = posts(:one)
-    visit post_path(post)
+feature 'Commenting on posts' do
+ 
+  feature 'Logged in users' do
 
-    within('#new_comment') do
-      fill_in "Content", with: "Cool story bro!"
-      click_on 'Comment'
+    before do
+      test_sign_in(:editor)
+      post = posts(:one)
+      visit post_path(post)
     end
 
-    page.must_have_content /success/i
-  end
-
-
-  scenario 'Adding a blank comment' do
-    test_sign_in :editor
-    post = posts(:one)
-    visit post_path(post)
-
-    within('#new_comment') do
-      fill_in "Content", with: ""
-      click_on 'Comment'
+    scenario 'can add a comment if valid' do
+      within('#new_comment') do
+        fill_in "Content", with: "Cool story bro!"
+        click_on 'Comment'
+      end
+      page.must_have_content /success/i
     end
 
-    page.must_have_content "Couldn't create comment."
-  end
+    scenario 'cannot add a blank comment' do
+      within('#new_comment') do
+        fill_in "Content", with: ""
+        click_on 'Comment'
+      end
+      page.must_have_content "Couldn't create comment."
+    end
+    
+  end 
 
-  scenario "Visitors who aren't logged in should not see the add comment form" do
-    post = posts(:one)
-    visit post_path(post)
 
-    page.wont_have_css('#new_comment')
+  feature 'Visitors' do
+
+    scenario "should not see the add comment form" do
+      post = posts(:one)
+      visit post_path(post)
+      page.wont_have_css('#new_comment')
+    end
+
   end
 
 end
